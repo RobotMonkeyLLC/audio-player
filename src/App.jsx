00670ffdad416afbd6a9e1cc7e81e37968dc7436
epinedo <epinedo@robotmonkeyllc.com>
2024-07-10 21:40:18 -0400

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Controls from './Controls.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [songs, setSongs] = useState([])
+  
+  useEffect(() => {
+    fetch('https://playground.4geeks.com/sound/songs').then((response) => response.json()).then((data) => {
+      setSongs(data.songs)
+      console.log(songs)
+    } )
+  }, [])
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {songs.map((song, index) => {
+        return (
+          <div key={index} className='row'>
+            <div className='col-1'>{index}</div>
+            <div className='col-7 pe-5'>{song.name}</div>
+            <div className=''></div>
+          </div>
+        )
+      })}
+      <Controls />
+      
     </>
   )
 }
---
+++ src/Controls.jsx
@@ -0,0 +1,11 @@
+export default function Controls() {
+    return (
+        <div className='row'>
+            <div className='col-12'>
+                <button className='btn btn-primary'>Play</button>
+                <button className='btn btn-primary'>Pause</button>
+                <button className='btn btn-primary'>Stop</button>
+            </div>
+        </div>
+    )
+}
