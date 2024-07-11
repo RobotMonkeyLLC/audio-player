525c07e4ec3ee9432a7be09d5405aafd57207c75
epinedo <epinedo@robotmonkeyllc.com>
2024-07-11 00:03:41 -0400

    playing not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Controls from './Controls.jsx'
 function App() {
   const [count, setCount] = useState(0)
   const [songs, setSongs] = useState([])
+  const [playing, setPlaying] = useState(false)
+  const [currentSong, setCurrentSong] = useState(null)
   
   useEffect(() => {
     fetch('https://playground.4geeks.com/sound/songs').then((response) => response.json()).then((data) => {
@@ -15,22 +17,36 @@ function App() {
     } )
   }, [])
 
+  useEffect(() => {
+    console.log('Playing:', playing)
+    document.querySelector('audio').play()
+  }, [playing])
   
-
+  const playSong = (e) => {
+    console.log(e)
+    console.log('Playing song', e.target.attributes.href.value)
+    let songUrl = ('https://playground.4geeks.com'+e.target.attributes.href.value)
+    //let audio = new Audio(songUrl)
+    console.log(songUrl)  
+    setCurrentSong(songUrl)
+    setPlaying(true)
+  }
   return (
-    <>
+    <div className='col-12'>
       {songs.map((song, index) => {
         return (
-          <div key={index} className='row'>
-            <div className='col-1'>{index}</div>
-            <div className='col-7 pe-5'>{song.name}</div>
-            <div className=''></div>
+          <div key={index} className='row song' href={song.url} onClick={e => playSong(e)}>
+            <div className='col-1' href={song.url}>{index}</div>
+            <div className='col-11' href={song.url}>{song.name}</div>            
           </div>
         )
       })}
-      <Controls />
-      
-    </>
+      <Controls 
+        setPlaying={setPlaying} 
+        playing={playing} 
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}/>
+    </div>
   )
 }
 
--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,10 +1,17 @@
-export default function Controls() {
+
+export default function Controls({setPlaying, playing, currentSong, setCurrentSong}) {
     return (
-        <div className='row'>
-            <div className='col-12'>
-                <button className='btn btn-primary'>Play</button>
-                <button className='btn btn-primary'>Pause</button>
-                <button className='btn btn-primary'>Stop</button>
+        <div className='row controls col-12 '>
+            <div className='bg-secondary'>
+                <button className='btn btn-primary'><i className="fa-solid fa-backward"></i></button>
+                <button className='btn btn-primary' onClick={() => setPlaying(!playing)}>{playing ? 
+                    <i className="fa-solid fa-pause"></i> : 
+                    <i className="fa fa-play" aria-hidden="true"></i>}
+                </button>
+                <button className='btn btn-primary'><i className="fa-solid fa-forward"></i></button>
+                <audio controls>
+                    <source src={currentSong} type="audio/mpeg"/>
+                </audio>
             </div>
         </div>
     )
