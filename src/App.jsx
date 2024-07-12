55cc3f918f00e684d3dad4b9a7c78dec6cefb3d4
epinedo <epinedo@robotmonkeyllc.com>
2024-07-11 22:27:54 -0400

    wokring player

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,58 @@ import './App.css'
 import Controls from './Controls.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let baseURL = 'https://playground.4geeks.com'
+
   const [songs, setSongs] = useState([])
   const [playing, setPlaying] = useState(false)
   const [currentSong, setCurrentSong] = useState(null)
+  const [currentId, setCurrentId] = useState(1)
   const [nextSong, setNextSong] = useState(null)
   const [prevSong, setPrevSong] = useState(null)
   
   useEffect(() => {
-    fetch('https://playground.4geeks.com/sound/songs').then((response) => response.json()).then((data) => {
+    fetch(baseURL+'/sound/songs').then((response) => response.json()).then((data) => {
       setSongs(data.songs)
       console.log(songs)
     } )
   }, [])
 
   useEffect(() => {
-    console.log('Playing:', playing)
     playing ? 
       document.querySelector('audio').play() : 
-      document.querySelector('audio').pause()
-      
-  }, [playing, currentSong])
-  
-  const playSong = (e) => {
-    //console.log('Playing song:', e.target.attributes.songID.value)
-    let songID = e.target.attributes.songid.value || null
-    console.log(songs[songID-1])
-    let songUrl = ('https://playground.4geeks.com'+songs[songID-1].url)
-    let prevSong = songID > 1 ? ('https://playground.4geeks.com'+songs[songID-2].url) : ('https://playground.4geeks.com'+songs[songs.length-1].url)
-    let nextSong = songID > songs.length-1 ? ('https://playground.4geeks.com'+songs[0].url) : ('https://playground.4geeks.com'+songs[songID].url)
-    songUrl === currentSong ? setPlaying(!playing) :
-    setCurrentSong(songUrl)
-    setPrevSong(prevSong)
-    setNextSong(nextSong)
-    console.log('Current song:', currentSong, 'Next song:', nextSong, 'Prev song:', prevSong)
-    document.querySelector('audio').load()
-    setPlaying(true)
+      document.querySelector('audio').pause()    
+    
+  }, [playing, currentId])
+
+  const toggleActive = (id) => {
+    let songs = document.querySelectorAll('.song')
+    let songID = id
+
+    songs.forEach(song => song.classList.remove('active'))
+    
+    let row = document.querySelector('.song [songid="'+songID+'"]')
+    row.parentElement.classList.add('active')
   }
+
+  const playSong = (id) => {
+    //console.log('Playing song:', e.target.attributes.songID.value)
+    let songID = id > songs.length ? 1 : id < 1 ? songs.length : id
+    setCurrentId(songID)
+    console.log(id)
+    let songUrl = (baseURL+songs[songID-1].url)
+    
+    currentSong === songUrl ? setPlaying(!playing) : setPlaying(true)
+    
+    setCurrentSong(songUrl)
+    document.querySelector('audio').load()
+    toggleActive(songID)
+  }
+  
   return (
-    <div className='col-12'>
+    <div className='col-12 pb-5'>
       {songs.map((song, index) => {
         return (
-          <div key={index} className='row song' onClick={e => playSong(e)}>
+          <div key={index} className='row song' songid={song.id} onClick={() => {playSong(song.id)}}>
             <div className='col-1' href={song.url} songid={song.id}>{song.id}</div>
             <div className='d-flex col-11' href={song.url} songid={song.id}>{song.name}</div>            
           </div>
@@ -56,7 +66,11 @@ function App() {
         setPlaying={setPlaying} 
         playing={playing} 
         currentSong={currentSong}
-        setCurrentSong={setCurrentSong}/>
+        currentId={currentId}
+        setCurrentId={setCurrentId}
+        nextSong={nextSong}
+        prevSong={prevSong}
+        playSong={playSong}/>
     </div>
   )
 }
--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,15 +1,17 @@
 
-export default function Controls({setPlaying, playing, currentSong, setCurrentSong}) {
+export default function Controls({setPlaying, playing, currentSong, currentId,setCurrentId, nextSong, prevSong, playSong}) {
     return (
         <div className='row controls col-12 bg-secondary py-3 justify-content-center'>
             <div className='col-2 d-flex justify-content-between'>
-                <button className='btn btn-primary'><i className="fa-solid fa-backward"></i></button>
+                <button className='btn btn-primary' onClick={()=>playSong(currentId-1)}>
+                    <i className="fa-solid fa-backward"></i>
+                </button>
                 <button className='btn btn-primary' onClick={() => setPlaying(!playing)}>{playing ? 
                     <i className="fa-solid fa-pause"></i> : 
                     <i className="fa fa-play" aria-hidden="true"></i>}
                 </button>
-                <button className='btn btn-primary'><i className="fa-solid fa-forward"></i></button>
-                <p>{currentSong}</p>
+                <button className='btn btn-primary' onClick={()=>playSong(currentId+1)}><i className="fa-solid fa-forward"></i></button>
+                
                 <audio>
                     <source src={currentSong} type="audio/mpeg"/>
                 </audio>
